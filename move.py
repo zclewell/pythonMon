66cfc4b819e4d6496a914f1080ce89e7829cd142
zach <zclewell@gmail.com>
2019-11-19 21:54:03 -0600

    Got rid of Move base class, implemented first pass for status move

--- move.py
+++ move.py
@@ -4,29 +4,34 @@ from monTypes import MonTypes
 from effective import how_effective
 
 
-class Move(ABC):
+class StatusMove:
+    def __init__(self, moveType, statusChanges):
+        self.moveType = moveType
+        self.statusChanges = statusChanges
+
+    def use(self, user, target, environment):
+        assert self in user.moves
+        target.stats['health'] *= self.statusChanges['health']
+        target.stats['speed'] *= self.statusChanges['speed']
+        target.stats['attack'] *= self.statusChanges['attack']
+        target.stats['defense'] *= self.statusChanges['defense']
+        target.stats['special_attack'] *= self.statusChanges['special_attack']
+        target.stats['special_defense'] *= self.statusChanges['special_defense']
+
+
+class AttackMove:
     def __init__(self, moveType, moveStats):
         assert isinstance(moveType, MonTypes)
         self.moveType = moveType
         self.moveStats = moveStats
 
-    def use(self, user, opponent,  environment):
-        raise NotImplementedError
-
-
-class StatusMove(Move):
-    def use(self, user, opponent, environment):
-        raise NotImplementedError
-
-
-class AttackMove(Move):
-    def use(self, user, opponent, environment):
+    def use(self, user, target, environment):
         assert self in user.moves
 
         # https://bulbapedia.bulbagarden.net/wiki/Damage
         damage = (2 * user.level / 5) + 2
         damage *= self.moveStats['base_power'] * user.stats[self.attack_name] / \
-            opponent.stats[self.defense_name]
+            target.stats[self.defense_name]
         damage = damage / 50 + 2
 
         modifier = 1
@@ -36,24 +41,24 @@ class AttackMove(Move):
             modifier *= 1.5
 
         # effectiveness
-        for monType in opponent.monTypes:
+        for monType in target.monTypes:
             modifier *= how_effective(self.moveType, monType)
 
-        health = opponent.stats['health'] - damage * modifier
+        health = target.stats['health'] - damage * modifier
         if health < 0:
             health = 0
-        opponent.stats['health'] = int(health)
+        target.stats['health'] = int(health)
 
 
 class PhysicalMove(AttackMove):
     def __init__(self, moveType, moveStats):
         self.attack_name = 'attack'
         self.defense_name = 'defense'
-        Move.__init__(self, moveType, moveStats)
+        AttackMove.__init__(self, moveType, moveStats)
 
 
 class SpecialMove(AttackMove):
     def __init__(self, moveType, moveStats):
         self.attack_name = 'special_attack'
         self.defense_name = 'special_defense'
-        Move.__init__(self, moveType, moveStats)
+        AttackMove.__init__(self, moveType, moveStats)
