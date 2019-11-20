094e603b9d2c7740045387739eb605dca00551b4
zach <zclewell@gmail.com>
2019-11-19 21:16:00 -0600

    Finished effective.py

--- effective.py
+++ effective.py
@@ -7,7 +7,7 @@ def how_effective(move_type, opp_type):
 
     super_effective = 2
     effective = 1
-    not_very_effective =  0.5
+    not_very_effective = 0.5
     not_effective = 0
 
     if opp_type is MonTypes.Normal:
@@ -96,7 +96,7 @@ def how_effective(move_type, opp_type):
         if move_type is MonTypes.Water:
             return super_effective
         if move_type is MonTypes.Grass:
-            return  super_effective
+            return super_effective
 
     if opp_type is MonTypes.Bug:
         if move_type is MonTypes.Fighting:
@@ -175,7 +175,7 @@ def how_effective(move_type, opp_type):
             return not_very_effective
         if move_type is MonTypes.Fairy:
             return not_very_effective
-    
+
     if opp_type is MonTypes.Water:
         if move_type is MonTypes.Steel:
             return not_very_effective
@@ -210,6 +210,82 @@ def how_effective(move_type, opp_type):
         if move_type is MonTypes.Ice:
             return super_effective
 
+    if opp_type is MonTypes.Electric:
+        if move_type is MonTypes.Flying:
+            return not_very_effective
+        if move_type is MonTypes.Ground:
+            return super_effective
+        if move_type is MonTypes.Steel:
+            return not_very_effective
+        if move_type is MonTypes.Electric:
+            return not_very_effective
 
+    if opp_type is MonTypes.Psychic:
+        if move_type is MonTypes.Fighting:
+            return not_very_effective
+        if move_type is MonTypes.Bug:
+            return super_effective
+        if move_type is MonTypes.Ghost:
+            return super_effective
+        if move_type is MonTypes.Psychic:
+            return not_very_effective
+        if move_type is MonTypes.Dark:
+            return super_effective
+
+    if opp_type is MonTypes.Ice:
+        if move_type is MonTypes.Fighting:
+            return super_effective
+        if move_type is MonTypes.Rock:
+            return super_effective
+        if move_type is MonTypes.Steel:
+            return super_effective
+        if move_type is MonTypes.Fire:
+            return super_effective
+        if move_type is MonTypes.Ice:
+            return not_very_effective
+
+    if opp_type is MonTypes.Dragon:
+        if move_type is MonTypes.Fire:
+            return not_very_effective
+        if move_type is MonTypes.Water:
+            return not_very_effective
+        if move_type is MonTypes.Grass:
+            return not_very_effective
+        if move_type is MonTypes.Electric:
+            return not_very_effective
+        if move_type is MonTypes.Ice:
+            return not_very_effective
+        if move_type is MonTypes.Dragon:
+            return not_very_effective
+        if move_type is MonTypes.Fairy:
+            return super_effective
+
+    if opp_type is MonTypes.Dark:
+        if move_type is MonTypes.Fighting:
+            return super_effective
+        if move_type is MonTypes.Bug:
+            return super_effective
+        if move_type is MonTypes.Ghost:
+            return not_very_effective
+        if move_type is MonTypes.Psychic:
+            return not_effective
+        if move_type is MonTypes.Dark:
+            return not_very_effective
+        if move_type is MonTypes.Fairy:
+            return super_effective
+
+    if opp_type is MonTypes.Fairy:
+        if move_type is MonTypes.Fighting:
+            return not_very_effective
+        if move_type is move_type.Poison:
+            return super_effective
+        if move_type is MonTypes.Bug:
+            return not_very_effective
+        if move_type is MonTypes.Steel:
+            return super_effective
+        if move_type is MonTypes.Dragon:
+            return not_effective
+        if move_type is MonTypes.Dark:
+            return not_very_effective
 
     return effective
